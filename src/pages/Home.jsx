d145c9fb83554c56abdc069aaeab84427549d880
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-27 12:13:31 -0700

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     <div className="home">
     <h4>Based out of Portland, OR</h4>
     <img className="portland-image" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1e/6e/19/2a/caption.jpg?w=700&h=500&s=1" alt="Portland, OR" />
+    <h4><a href="https://github.com/Vanillajaviscript/react-portfolio-backend">Backend code for this portfolio project on Github</a></h4>
     </div>
   )
 }
